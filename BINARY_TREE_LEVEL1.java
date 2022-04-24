a1bcd142855ffd401ca397cb14585411d7362f05
SATYARTH TIWARI <78496068+Satyarth007@users.noreply.github.com>
2022-04-24 23:28:21 +0530

    Create BINARY_TREE_LEVEL1.java

---
+++ BINARY_TREE_LEVEL1.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class Binary_Tree_Demo1 {
+	//Node class
+	static class Node{
+		int data;
+		Node left;
+		Node right;
+		 Node(int data){
+			 this.data=data;
+			 this.left=null;
+			 this.right=null;
+		 }
+	}
+	//Binary tree formation using the user array input
+	static class BinaryTree{
+		static int idx=-1;
+		public static Node buildTree(int nodes[]) {
+			idx++;
+			if(nodes[idx]==-1) {
+				return null;
+			}
+			 Node newNode=new Node(nodes[idx]);
+			 newNode.left=buildTree(nodes);
+			 newNode.right=buildTree(nodes);
+			 
+			return newNode;
+			 
+			
+		}
+	}
+//===============================PREORDER TRAVESAL CODE=====================================================
+	public static void preorder(Node root) {
+		if(root==null) {
+			return;
+		}
+		System.out.print(root.data+ " ");
+		preorder(root.left);
+		preorder(root.right);
+		
+	}
+//==========================================================================================================	
+	public static void main(String[] args) {
+		// LEARNING BINARY TREE
+		int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; //WE CAN MAKE DYNAMIC INPUT WITH ARRAYS USING SCANNER CLASS
+		BinaryTree tree=new BinaryTree();
+		Node root=tree.buildTree(nodes);   // GETTING ROOT DATA BY BUILDING TREE
+		//System.out.println(root.data);   // CODE TO TEST ROOT DATA
+		preorder(root);                    //CALLING PREORDER TRAVESAL CODE
+
+	}
+
+}
